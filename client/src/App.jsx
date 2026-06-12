43bdb04ddf6ac783f7246098813bf5dabc16efb3
Amit Kumar Singh <amitkumar798558@gmail.com>
2026-06-13 02:17:50 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-agent-b4t8.onrender.com"
+export const ServerUrl  = "https://ai-interview-agent-f.onrender.com"
 
 function App() {
 
